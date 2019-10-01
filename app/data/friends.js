5be406f3c6f8caf38496af22de05de5ab841c4d8
Drew Reynolds <drew@sportscrm.com>
2019-09-30 23:12:08 -0500

    working app

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friends = [
+    {
+        "name": "John",
+        "photo": "https://cdn3.vectorstock.com/i/1000x1000/92/77/faceless-businessman-avatar-man-in-suit-with-blue-vector-13569277.jpg",
+        "scores": [
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+        ]
+    },
+    {
+        "name": "Adam",
+        "photo": "https://cdn1.vectorstock.com/i/1000x1000/06/70/brunette-businessman-avatar-man-face-profile-icon-vector-21960670.jpg",
+        "scores": [
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+        ]
+    },
+    {
+        "name": "Jennifer",
+        "photo": "https://previews.123rf.com/images/juliasart/juliasart1704/juliasart170400022/75406270-vector-girl-icon-woman-avatar-face-icon-cartoon-style-.jpg",
+        "scores": [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+        ]
+    },
+    {
+        "name": "Ashley",
+        "photo": "https://cdn1.iconfinder.com/data/icons/avatars-1-5/136/87-512.png",
+        "scores": [
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+        ]
+    },
+    {
+        "name": "Bradley",
+        "photo": "https://image.shutterstock.com/image-vector/man-character-face-avatar-glasses-260nw-562077406.jpg",
+        "scores": [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+        ]
+    },
+];
+
+module.exports = friends;
